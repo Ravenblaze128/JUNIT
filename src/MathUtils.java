120fb99d5f3775548fdfbc5f8026bc0dbee698ae
Prattipati Sri Raviteja <sprattipati1@toromail.csudh.edu>
2024-02-06 21:15:10 -0800

    Test again

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -1,3 +1,4 @@
+package src;
 public class MathUtils {
 
     public int add(int a, int b) {
--- src/MathUtilsTest.java
+++ src/MathUtilsTest.java
@@ -1,6 +1,8 @@
+package src;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MathUtilsTest {
